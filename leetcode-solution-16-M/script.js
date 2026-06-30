21c06d24a83646f36f848e4d09d05b462cdf9439
noceur06 <ya0558494@gnail.com>
2026-06-30 17:21:51 +0530

    Add leetcode solution 16 m

---
+++ leetcode-solution-16-M/script.js
@@ -0,0 +1,33 @@
+// Given a string s consisting only of characters a, b and c.
+
+// Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+// Example 1:
+
+// Input: s = "abcabc"
+// Output: 10
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again).
+// Example 2:
+
+// Input: s = "aaacb"
+// Output: 3
+// Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb".
+// Example 3:
+
+// Input: s = "abc"
+// Output: 1
+
+// explain me this and what should i know to solve this and how can i solve this tell me with what i can solve these with steps
+
+let numberOfStrings = function (s) {
+  let count = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i; j < s.length; j++) {
+      let sub = s.slice(i, j + 1);
+      if (sub.includes("a") && sub.includes("b") && sub.includes("c")) {
+        count++;
+      }
+    }
+  }
+  return count;
+};
