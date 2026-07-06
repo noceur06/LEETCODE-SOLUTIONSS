bdac412f1cc9221e761ff3dd6812bdab94eae10d
noceur06 <ya0558494@gnail.com>
2026-07-07 01:54:47 +0530

    Add all leetcode solutions

--- leetcode-solution-16-M/script.js
+++ leetcode-solution-16-M/script.js
@@ -31,3 +31,30 @@ let numberOfStrings = function (s) {
   }
   return count;
 };
+
+// REAL SOLUTION FOR LEETCODE
+
+
+// var numberOfSubstrings = function (s) {
+//     let count = 0;
+//     let left = 0;
+
+//     let freq = {
+//         a: 0,
+//         b: 0,
+//         c: 0
+//     };
+
+//     for (let right = 0; right < s.length; right++) {
+//         freq[s[right]]++;
+
+//         while (freq.a > 0 && freq.b > 0 && freq.c > 0) {
+//             count += s.length - right;
+
+//             freq[s[left]]--;
+//             left++;
+//         }
+//     }
+
+//     return count;
+// };
