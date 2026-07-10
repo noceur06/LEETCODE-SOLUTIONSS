8e803a8dc75742ce9842eedea61add66c48a0202
Sayyed Yasar Ali <ya0558494@gmail.com>
2026-07-10 15:41:06 +0530

    Enhance checkIfInstanceOf with null/undefined checks
    Added checks for null and undefined in checkIfInstanceOf function.

--- leetcode-solution-27/script.js
+++ leetcode-solution-27/script.js
@@ -39,12 +39,13 @@
  * @return {boolean}
  */
 var checkIfInstanceOf = function (obj, classFunction) {
-  if (typeof classFunction !== "function") {
+  if (typeof classFunction !== "function" || obj === null || obj === undefined) {
     return false;
   }
 
   return Object(obj) instanceof classFunction;
 };
+
 
 /**
  * checkIfInstanceOf(new Date(), Date); // true
