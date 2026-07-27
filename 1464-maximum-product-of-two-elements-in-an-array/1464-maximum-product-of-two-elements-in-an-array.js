7f5850e9279c0b8e4bed636b3342263033570252
Sayyed Yasar Ali <ya0558494@gmail.com>
2026-07-27 17:10:19 +0530

    Time: 2 ms (55.36%), Space: 54.1 MB (66.52%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -3,14 +3,21 @@
  * @return {number}
  */
 var maxProduct = function(nums) {
-    let prod = 0;
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            let max = (nums[i] - 1) * (nums[j] -1);
-            if(max > prod){
-                prod = max;
-            }
-        }
+    let arr = nums.sort((a,b) => b - a);
+    let final = 0;
+    let result = (arr[0] - 1) * (arr[1] -1);
+    if(result > final){
+        final = result;
     }
-    return prod;
+    return final;
+    // let prod = 0;
+    // for(let i = 0; i < nums.length; i++){
+    //     for(let j = i + 1; j < nums.length; j++){
+    //         let max = (nums[i] - 1) * (nums[j] -1);
+    //         if(max > prod){
+    //             prod = max;
+    //         }
+    //     }
+    // }
+    // return prod;
 };
