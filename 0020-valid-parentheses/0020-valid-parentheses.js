95ff5605f353665b3c6a18837c1a521b0f4fc8c5
Sayyed Yasar Ali <ya0558494@gmail.com>
2026-08-30 18:16:08 +0530

    Time: 4 ms (51.43%), Space: 54.8 MB (81.91%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = [];
+
+    for(let i = 0; i < s.length; i++){
+
+        if(s[i] === "(" || s[i] === "{" || s[i] === "["){
+            stack.push(s[i]);
+        }else{
+            let last = stack.pop();
+
+            if((s[i] === ")" && last !== "(") ||
+           ( s[i] === "}" && last !== "{") ||
+            ( s[i] === "]" && last !== "[")){
+                return false;
+            }
+        }
+    }
+     return stack.length === 0;
+};
