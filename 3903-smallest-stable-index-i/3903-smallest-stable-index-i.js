fe4c76663da306b0e0cdef358535b30dde9f720f
Sayyed Yasar Ali <ya0558494@gmail.com>
2026-09-05 15:33:55 +0530

    Time: 0 ms (100%), Space: 57.1 MB (59.26%) - LeetHub

---
+++ 3903-smallest-stable-index-i/3903-smallest-stable-index-i.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var firstStableIndex = function(nums, k) {
+    for(let i = 0; i < nums.length; i++){
+        let max = nums[0];
+        for(let j = 0; j < i; j++){
+            max = Math.max(max,nums[j]);
+        }
+        let min = nums[i];
+        for(let j = i; j < nums.length; j++){
+            min = Math.min(min,nums[j]);
+        }
+        let score = max - min;
+
+        if(score <= k){
+            return i;
+        }
+    }
+    return -1
+};
